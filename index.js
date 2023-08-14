858bf538dd57a72de1754bbd214bccba1a7779d1
Tony Glass <idttip.tg@gmail.com>
2023-08-14 14:21:39 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee= {
+    name: "Tony",
+    streetAddress: "123 street"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const copyOfEmployee= {
+        ...employee
+    }
+    copyOfEmployee[key] = value
+    return copyOfEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee   
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee= {
+        ...employee
+    }
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
